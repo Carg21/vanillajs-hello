27899b3c90f781d5ac58d8549ceebc66b2ea887e
Carg21 <carlosgv512@hotmail.com>
2024-10-23 13:38:43 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indiwho = Math.floor(Math.random() * who.length);
+  let indiaction = Math.floor(Math.random() * action.length);
+  let indiwhat = Math.floor(Math.random() * what.length);
+  let indiwhen = Math.floor(Math.random() * when.length);
+
+  let excusegen = document.querySelector("#excuse");
+  console.log(excusegen);
+  excusegen.innerHTML =
+    who[indiwho] +
+    " " +
+    action[indiaction] +
+    " " +
+    what[indiwhat] +
+    " " +
+    when[indiwhen];
 };
